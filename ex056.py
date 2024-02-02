e6bec3a2d7fe559aa020e1341d02f0a2722c3957
Thayso Weslley <92331482+Thayso-Weslley@users.noreply.github.com>
2024-02-02 15:34:20 -0300

    Exercícios de Python

--- ex056.py
+++ ex056.py
@@ -14,33 +14,42 @@ while True:
     try:
         while True: 
             titulo("ANALISE DAS PESSOAS")
-            
+
             somaIdade = 0
             médiaIdade = 0
             maiorIdadeHomem = 0
             nomeMaisVelho = ""
+            mulheresComMenosde20 = 0
 
             for n in range(1, 5):
                 print("----- {}ª PESSOA -----".format(n))
                 nome = str(input("Nome: ")).strip()
                 idade = int(input("Idade: "))
                 sexo = str(input("Sexo [M/F]: ")).strip().upper()
+                while sexo != "M" and sexo != "F":
+                    sexo = str(input("Opcão inválida!\nDigite M ou F: "))
 
                 somaIdade += idade
 
                 if n == 1 and sexo == "M":
                     maiorIdadeHomem = idade
                     nomeMaisVelho = nome
+                else:
+                    if idade > maiorIdadeHomem and sexo == "M":
+                        maiorIdadeHomem = idade
+                        nomeMaisVelho = nome
 
-                médiaIdade = somaIdade / 4
                 if sexo == "F":
                     if idade < 20:
-                        
-    
+                        mulheresComMenosde20 += 1
+
+
+            médiaIdade = somaIdade / 4
+            print("\n\n   A média de idade do grupo é {}\n   O homem mais velho tem {} anos e se chama {}\n   Há {} mulhere(s) com menos de 20 anos\n\n".format(médiaIdade, maiorIdadeHomem, nomeMaisVelho, mulheresComMenosde20))
             break
         break
     except:
         system("cls")
         print("   {}ERRO DETECTADO!{}".format(vermelho, normal))
         print("   {}Por favor, coloque os dados de entrada corretamente para que o programa funcione.\n   Lembre-se de escrever apenas números. Se for um número decimal, use ponto no lugar da vírgula.{}\n\n".format(amarelo, normal))
-        continue
+        continue
